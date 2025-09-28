0ac726cec52acba24c740ba60402820cc2682c50
Suraj Sharma <surajsharma@Mac.mshome.net>
2025-09-28 22:18:26 +0530

    step-2 scaliton of routes

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+
+app.post('/user/signup',(req,res)=>{
+
+})
+
+app.post('/user/login',(req,res)=>{
+    
+})
+
+app.get('/user/purchases',(req,res)=>{
+    
+})
+
+app.get('/courses',(req,res)=>{
+    
+})
+
+app.post('/course/purchse',(req,res)=>{
+    
+})
