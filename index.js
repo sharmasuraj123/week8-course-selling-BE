e965ad656f51f927fffee016592e69962cb8731a
Suraj Sharma <surajsharma@Surajs-MacBook-Air.local>
2025-10-02 17:38:54 +0530

    adding .env and .gitignore file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const monogoURI = process.env.Mongodb_URI;
 
 const { userRouter } = require("./Routes/user");
 const { courseRouter } = require("./Routes/courses");
-const { adminRouter } = require("/Routes/admin");
+const { adminRouter } = require("./Routes/admin");
 const app = express();
 
 app.use("/api/v1/user", userRouter);
@@ -11,7 +13,7 @@ app.use("api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
 async function main() {
-  await mongoose("");
+  await mongoose(monogoURI);
 
   app.listen(3000, () => {
     console.log("server is running on port 3000");
